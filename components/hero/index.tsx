f7b083854c2d21fd28f7253b7cc16e44bdc3a2b6
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-06-05 23:18:58 +0630

    update: responsive title(hero section)

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -12,15 +12,18 @@ export default function Hero() {
         className="mb-10 w-[340px]"
       />
 
-      <h1 className="flex flex-wrap items-center text-2xl font-bold text-muted-foreground xs:text-3xl sm:text-4xl">
-        Hi <Icons.wavingHand className="mb-[10px] inline-block h-10 w-10" />,
-        <span className="ml-2">I am</span>
-        <span className="ml-2 font-extrabold text-foreground">
+      <h1 className="flex flex-col items-center space-y-2 text-3xl font-bold text-muted-foreground xs:flex-row xs:space-y-0">
+        <span className="flex">
+          Hi
+          <Icons.wavingHand className="ml-1 inline-block h-8 xs:w-8" />,
+          <span className="ml-2">I am</span>
+        </span>
+        <span className="font-extrabold text-foreground xs:ml-2">
           Zwel Htet Yan
         </span>
       </h1>
 
-      <div className="mt-7 rounded bg-muted  px-4 py-2 text-center sm:py-3 sm:text-lg">
+      <div className="mt-10 rounded-md border bg-muted px-4 py-2 text-center sm:py-3 sm:text-lg">
         A passionate fullstack developer based in Myanmar
       </div>
     </section>
