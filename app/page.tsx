a6d98400ec6b24dffbeaf6ea99b9db17a04a49d4
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-06-07 14:16:03 +0630

    chore: remove unnecessary commented codes

--- app/page.tsx
+++ app/page.tsx
@@ -3,34 +3,6 @@ import { About, Contact, Hero, Projects } from "@/components";
 export default function IndexPage() {
   return (
     <div className="container max-w-2xl px-4 xs:px-8">
-      {/* <div className="flex max-w-[980px] flex-col items-start gap-2 pt-24">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Beautifully designed components <br className="hidden sm:inline" />
-          built with Radix UI and Tailwind CSS.
-        </h1>
-        <p className="max-w-[700px] text-lg text-muted-foreground">
-          Accessible and customizable components that you can copy and paste
-          into your apps. Free. Open Source. And Next.js 13 Ready.
-        </p>
-      </div>
-      <div className="flex gap-4">
-        <Link
-          href={siteConfig.links.docs}
-          target="_blank"
-          rel="noreferrer"
-          className={buttonVariants()}
-        >
-          Documentation
-        </Link>
-        <Link
-          target="_blank"
-          rel="noreferrer"
-          href={siteConfig.links.github}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          GitHub
-        </Link>
-      </div> */}
       <Hero />
       <About />
       <Projects />
