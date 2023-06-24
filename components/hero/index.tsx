327b268f1d85dae14aa2f73ab1a11c2675d02567
zwelhtetyan <zwelhtetyan2001@gmail.com>
2023-06-24 11:11:07 +0630

    impv(performance): remove animate emoji for better performance

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Icons } from "../icons";
+// import { Icons } from "../icons";
 
 export default function Hero() {
   return (
@@ -15,8 +15,8 @@ export default function Hero() {
 
       <h1 className="flex flex-col items-center space-y-2 text-3xl font-bold text-muted-foreground xs:flex-row xs:space-y-0">
         <span className="flex">
-          Hi
-          <Icons.wavingHand className="ml-1 inline-block h-8 w-8" />,
+          Hi 👋 ,
+          {/* <Icons.wavingHand className="ml-1 inline-block h-8 w-8" />, */}
           <span className="ml-3">I am</span>
         </span>
         <span className="font-extrabold text-foreground xs:ml-2">
